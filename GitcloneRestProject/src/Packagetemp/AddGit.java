bb21efbbd9e8908d21906c94534d9b8f071968f0
sumak <sumak@192.168.0.16>
2022-04-19 13:13:01 -0400

    Adding Addgit.java file.

---
+++ GitcloneRestProject/src/Packagetemp/AddGit.java
@@ -0,0 +1,14 @@
+package Packagetemp;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class AddGit {
+
+	public static void main(String[] args) {
+
+		System.out.println("Adding to git hub");
+		
+		
+	}
+
+}
